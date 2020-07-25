b3a1ddfae582f790b5108b2e93515c53f8db7079
kinker31 <dp223171@gmail.com>
2020-07-25 10:34:23 -0500

    conf.lua changes

--- conf.lua
+++ conf.lua
@@ -2,5 +2,7 @@ function love.conf(t)
     t.window.title = "Red Recreation"
     t.window.icon = "icon.png"
     t.version = "11.3"
-    t.modules.mouse = true 
+    t.identity = "r_recreate"
+    t.window.width = 1024
+    t.window.height = 768
 end
--- main.lua
+++ main.lua
@@ -6,6 +6,6 @@ function love.draw()
  local DynamicSquare = require "dynamicBasicSquare"
  local StaticCircle = require "staticBasicCircle"
  local DynamicCircle = require "dynamicBasicCircle"
- local squareOne = StaticSquare.spawn(0, 1)
- local circleOne = StaticCircle.spawn(1, 0)
+ local squareOne = StaticSquare.spawn(0, 0)
+ local circleOne = StaticCircle.spawn(0, 0)
 end
